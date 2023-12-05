afc6f5a905bbce6f3e63f7decf6facaa7d7635f3
babaquero07 <brayanalexanderb@gmail.com>
2023-12-05 12:24:05 -0500

    feat(controller): first GET Request to get all users

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,1 +1,18 @@
-export const getAllUsers = () => {};
+import { NextFunction, Request, Response } from "express";
+import User from "../models/User.js";
+
+export const getAllUsers = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const users = await User.find();
+
+    return res.status(200).json({ users, total: users.length, message: "Ok" });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({ message: "Error", cause: error.message });
+  }
+};
