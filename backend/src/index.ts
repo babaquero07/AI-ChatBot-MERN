b72f61eb72e5031dc75e54c2eb39d5303cc20f6a
babaquero07 <brayanalexanderb@gmail.com>
2023-12-05 10:00:55 -0500

    config: setup MongoDB database connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello World");
+import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
+
+// Connections and listeners
+const PORT = process.env.PORT || 8090;
+
+connectToDatabase()
+  .then(() => {
+    app.listen(PORT, () => console.log("Server open & connected to Database"));
+  })
+  .catch((error) => {
+    console.log(error);
+    // process.exit(1);
+  });
