51074ad774773a508c82164f5c21c5912d095245
babaquero07 <brayanalexanderb@gmail.com>
2023-12-05 13:06:52 -0500

    feat(user-routes): User Signup POST Route To Store Users

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js";
+import { hash } from "bcrypt";
 
 export const getAllUsers = async (
   req: Request,
@@ -10,6 +11,33 @@ export const getAllUsers = async (
     const users = await User.find();
 
     return res.status(200).json({ users, total: users.length, message: "Ok" });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({ message: "Error", cause: error.message });
+  }
+};
+
+export const userSignup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "Missing fields" });
+    } else {
+      const hashedPassword = await hash(password, 10);
+
+      const user = new User({ name, email, password: hashedPassword });
+      await user.save();
+
+      return res
+        .status(201)
+        .json({ id: user._id.toString(), message: "User created" });
+    }
   } catch (error) {
     console.log(error);
 
