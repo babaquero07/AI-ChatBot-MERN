1a58d2defc61f7b1145015aa19b9b1906d7b4bb9
babaquero07 <brayanalexanderb@gmail.com>
2023-12-05 11:27:40 -0500

    feat(routes): setting up for users and chats

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import { config } from "dotenv";
+import morgan from "morgan";
+import appRouter from "./routes/index.js";
 
 config();
 const app = express();
 
 // Middlewares
 app.use(express.json());
+
+// Logger !todo remove in production
+app.use(morgan("dev"));
+
+app.use("/ap/v1", appRouter);
 
 export default app;
