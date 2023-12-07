75f6db11811822190d6873fef654d3aa8f1cc81a
babaquero07 <brayanalexanderb@gmail.com>
2023-12-07 12:41:35 -0500

    feat(auth): Added authentication context provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,21 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {BrowserRouter} from 'react-router-dom'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import App from "./App.tsx";
+import "./index.css";
 
-import {createTheme, ThemeProvider} from '@mui/material'
+import { createTheme, ThemeProvider } from "@mui/material";
 
 const theme = createTheme({
-  typography: {fontFamily: 'Roboto Slab, serif', allVariants: {color: 'white'}}
-})
+  typography: {
+    fontFamily: "Roboto Slab, serif",
+    allVariants: { color: "white" },
+  },
+});
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+    <AuthProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </AuthProvider>
+  </React.StrictMode>
+);
