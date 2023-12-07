3220d6e392a57af4405f7acc465a969719ec7665
babaquero07 <brayanalexanderb@gmail.com>
2023-12-07 13:07:03 -0500

    feat(navigation-links): added navigation links and styling to the header component

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,13 +1,52 @@
 import { AppBar, Toolbar } from "@mui/material";
 import Logo from "./shared/Logo";
+import NavigationLink from "./shared/NavigationLink";
+
+import { useAuth } from "../context/AuthContext";
 
 const Header = () => {
+  const auth = useAuth();
+
   return (
     <AppBar
       sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}
     >
       <Toolbar sx={{ display: "flex" }}>
         <Logo />
+        <div>
+          {auth?.isLoggedIn ? (
+            <>
+              <NavigationLink
+                to="/chat"
+                bg="#00fffc"
+                text="Go to chat"
+                textColor="black"
+              />
+              <NavigationLink
+                to="/"
+                bg="#51538f"
+                text="Logout"
+                textColor="#ffffff"
+                onClick={auth?.logout}
+              />
+            </>
+          ) : (
+            <>
+              <NavigationLink
+                to="/login"
+                bg="#00fffc"
+                text="Login"
+                textColor="black"
+              />
+              <NavigationLink
+                to="/signup"
+                bg="#51538f"
+                text="Signup"
+                textColor="#ffffff"
+              />
+            </>
+          )}
+        </div>
       </Toolbar>
     </AppBar>
   );
