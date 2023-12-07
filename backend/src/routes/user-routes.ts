b0ffd2c0fecb71b448e80a17033acf7d697a4a06
babaquero07 <brayanalexanderb@gmail.com>
2023-12-07 16:14:59 -0500

    fix(user-route): changed userRoutes.post to userRoutes.get for /auth-status

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -19,6 +19,6 @@ const userRoutes = Router();
 userRoutes.get("/", getAllUsers);
 userRoutes.post("/signup", validate(signupValidator), userSignup);
 userRoutes.post("/login", validate(loginValidator), userLogin);
-userRoutes.post("/auth-status", verifyToken, verifyUser);
+userRoutes.get("/auth-status", verifyToken, verifyUser);
 
 export default userRoutes;
