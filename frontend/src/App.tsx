68bb409a444ffb93c245257b82485bd2d68635e4
babaquero07 <brayanalexanderb@gmail.com>
2023-12-07 10:25:32 -0500

    feat(material-ui-&-routes): created a customized theme of Material UI & config routes with react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,25 @@
+import { Routes, Route } from 'react-router-dom'
+
+import Header from './components/Header'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Chat from './pages/Chat'
+import NotFound from './pages/Notfound'
+
 function App() {
 
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/chat" element={<Chat/>} />
+        <Route path="*" element={<NotFound/>} />
+      </Routes>
+    </main>
   )
 }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {BrowserRouter} from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 
+import {createTheme, ThemeProvider} from '@mui/material'
+
+const theme = createTheme({
+  typography: {fontFamily: 'Roboto Slab, serif', allVariants: {color: 'white'}}
+})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
