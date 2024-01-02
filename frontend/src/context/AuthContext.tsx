6b50c57225e1acb0e5e4f86d7427e7737a999c3c
babaquero07 <brayanalexanderb@gmail.com>
2024-01-02 14:24:29 -0500

    Add signup functionality to AuthContext and Signup page

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -10,6 +10,7 @@ import {
   checkAuthStatus,
   loginUser,
   logoutUser,
+  signupUser,
 } from "../helpers/api-communicator";
 
 type User = {
@@ -54,7 +55,14 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     }
   };
 
-  const signup = async (name: string, email: string, password: string) => {};
+  const signup = async (name: string, email: string, password: string) => {
+    const data = await signupUser(name, email, password);
+
+    if (data) {
+      setUser({ email: data.email, name: data.name });
+      setIsLoggedIn(true);
+    }
+  };
 
   const logout = async () => {
     await logoutUser();
--- frontend/src/helpers/api-communicator.ts
+++ frontend/src/helpers/api-communicator.ts
@@ -13,6 +13,24 @@ export const loginUser = async (email: string, password: string) => {
   return res.data;
 };
 
+export const signupUser = async (
+  name: string,
+  email: string,
+  password: string
+) => {
+  const res = await axios.post("/user/signup", {
+    name,
+    email,
+    password,
+  });
+
+  if (res.status !== 201) {
+    throw new Error("Error signing up");
+  }
+
+  return res.data;
+};
+
 export const checkAuthStatus = async () => {
   const res = await axios.get("/user/auth-status");
 
